911a441765ad94f9d809b623b91382b0796af727
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2023-05-30 21:02:41 +0900

    model 작업

---
+++ ppakcoding-todo/OpenChat/Model/MessageEntity.swift
@@ -0,0 +1,16 @@
+//
+//  MessageEntity.swift
+//  ppakcoding-todo
+//
+//  Created by WonDongHyun on 2023/05/30.
+//
+
+import Foundation
+
+struct MessageEntity {
+    var refId : String
+    var senderID : String
+    var senderNickname : String
+    var message : String
+    var createdAt : String
+}
